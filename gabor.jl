3835e4667c63fc2d1f1479038b768d10a6105479
Amisi Kiarie <amisikiarie@gmail.com>
2018-10-15 17:13:28 +0200

    yeashashdfadshf

--- gabor.jl
+++ gabor.jl
@@ -1,30 +1,67 @@
 module Gabor
 
 # Exceptions
-struct InvalidFunctionDimensions <: Exception
+struct InvalidFuncDimensions <: Exception
     funcSize::Int
     spaceSize::Int
 end
-Base.showerror(io::IO, e::InvalidFunctionDimensions) =
-    print(io, "The dimensions ($(e.funcSize)) of the Function values"
+Base.showerror(io::IO, e::InvalidFuncDimensions) =
+    print(io, "The dimensions ($(e.funcSize)) of the Func values"
           * " do not match the size ($(e.spaceSize)) of the space!")
 
+struct InvalidElemFuncDimensions <: Exception
+    spaceSize::Int
+    timeStep::Int
+    freqStep::Int
+end
+Base.showerror(io::IO, e::InvalidElemFuncDimensions) =
+    print(io, "Unable to fit time-step $(e.timeStep) and frequency-step"
+         * " of $(e.freqStep) in space of size $(e.spaceSize)!")
 
-scalar = Complex{Float64}
+
+Scalar = Complex{Float64}
 
 struct Space
-    size::Int
+    domain::UnitRange{Int64}
+end
+size(space::Space) = length(space.domain)
+
+struct Func
+    space::Space
+    values::Vector{Scalar}
+    function Func(space::Space, values::Vector{Scalar})
+        if length(values) ≠ size(space)
+            throw(InvalidFuncDimensions(length(values), size(space)))
+        end 
+        new(space, values)
+    end
 end
 
-struct Function
-    space::Space
-    values::Vector{scalar}
+# the shift-modulation operator on funcs
+function ψ(p::Int, q::Int, g::Func)
+    g_shift = circshift(g.values, p)
+    L = size(g.space)
+    Func(g.space, [exp(2π*im*q*k/L)*g_shift[k+1] for k in g.space.domain])
 end
-function Function(Space, values::Vector{scalar}) 
-    if length(values) != Space.size
-        throw(InvalidFunctionDimensions(length(values), Space.size))
+
+struct ElemFunc
+    func::Func
+    timeStep::Int
+    freqStep::Int
+    function ElemFunc(func::Func, timeStep::Int, freqStep::Int)
+        sizeDivisors = [size(func.space) % i for i in [timeStep, freqStep]]
+        nonDiv = filter(k -> k ≠ 0, sizeDivisors) # failed to divide evenly
+        if (timeStep ≤ 0) || (freqStep ≤ 0) || (length(nonDiv) ≠ 0)
+            throw(InvalidElemFuncDimensions(func.space.size, timeStep, freqStep))
+        end
+        new(func, timeStep, freqStep)
     end
-    Function(Space, values)
+end
+
+function operator(g::ElemFunc)
+    M, N = map(div -> Int(size(g.func.space)/div), [g.timeStep, g.freqStep])
+    shift_values = [ψ(m*g.timeStep, n*g.freqStep, g.func).values for m in 0:M-1, n in 0:N-1]
+    sum([ψg*transpose(ψg) for ψg in shift_values])
 end
 
 end
