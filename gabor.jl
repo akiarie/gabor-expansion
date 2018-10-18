1aaed0e59bd851d82c103ce8b16ffe93f3c5f8fc
Amisi Kiarie <amisikiarie@gmail.com>
2018-10-18 04:56:18 +0200

    progress

--- gabor.jl
+++ gabor.jl
@@ -1,5 +1,6 @@
 module Gabor
-
+import Base:*
+import Base.size
 using LinearAlgebra
 
 # Exceptions
@@ -26,31 +27,41 @@ Base.showerror(io::IO, e::FunctionsDoNotMatch) =
     print(io, "Cannot compare functions with different dimensions or"
          * " domains for biorthogonality")
 
+struct AnalyseMismatch <: Exception
+end
+Base.showerror(io::IO, e::AnalyseMismatch) =
+print(io, "Domain of function being analysed does not match"
+      * " elementary function domain")
+
+struct SynthesiseMismatch <: Exception
+end
+Base.showerror(io::IO, e::AnalyseMismatch) =
+print(io, "Size of coefficients does not match elementary function dimensions")
+
+
 
 Scalar = Complex{Float64}
 
-struct Space
-    domain::UnitRange{Int64}
-end
-size(space::Space)::Int = length(space.domain)
+periodic(k::Integer, P) = ((k%P)+P)%P
 
 struct Func
-    space::Space
+    domain::UnitRange{Int64}
     values::Vector{Scalar}
-    function Func(space::Space, values::Vector{Scalar})
-        if length(values) ≠ size(space)
-            throw(InvalidFuncDimensions(length(values), size(space)))
+    function Func(domain::UnitRange{Int64}, values::Vector{Scalar})
+        if length(values) ≠ length(domain)
+            throw(InvalidFuncDimensions(length(values), length(domain)))
         end 
-        new(space, values)
+        new(domain, values)
     end
 end
-(f::Func)(k::Int)::Scalar = f.values[findfirst(isequal(k), f.space.domain)]
+(f::Func)(k::Int)::Scalar = f.values[findfirst(isequal(periodic(k, length(f.domain))), f.domain)]
+*(A::Array{Gabor.Scalar}, f::Func) = Func(f.domain, A*f.values)
 
 # the shift-modulation operator on funcs
 function ψ(p::Int, q::Int, g::Func)::Func
-    g_shift = circshift(g.values, p)
-    L = size(g.space)
-    Func(g.space, [exp(2π*im*q*k/L)*g_shift[k+1] for k in g.space.domain])
+    g_shift = Func(g.domain, circshift(g.values, p))
+    L = length(g.domain)
+    Func(g.domain, [exp(2π*im*q*k/L)*g_shift(k) for k in g.domain])
 end
 
 struct ElemFunc
@@ -58,32 +69,33 @@ struct ElemFunc
     timeStep::Int
     freqStep::Int
     function ElemFunc(func::Func, timeStep::Int, freqStep::Int)
-        sizeDivisors = [size(func.space) % i for i in [timeStep, freqStep]]
+        sizeDivisors = [length(func.domain) % i for i in [timeStep, freqStep]]
         nonDiv = filter(k -> k ≠ 0, sizeDivisors) # failed to divide evenly
         if (timeStep ≤ 0) || (freqStep ≤ 0) || (length(nonDiv) ≠ 0)
-            throw(InvalidElemFuncDimensions(func.space.size, timeStep, freqStep))
+            throw(InvalidElemFuncDimensions(length(func.domain), timeStep, freqStep))
         end
         new(func, timeStep, freqStep)
     end
+    ElemFunc(f::Func, ψg::ElemFunc) = ElemFunc(f, ψg.timeStep, ψg.freqStep)
 end
 (ψg::ElemFunc)(m::Int, n::Int)::Func = ψ(m*ψg.timeStep, n*ψg.freqStep, ψg.func)
-dimensions(ψg::ElemFunc) = map(div -> Int(size(ψg.func.space)/div), [ψg.timeStep, ψg.freqStep])
+dimensions(ψg::ElemFunc) = vcat(map(div -> Int(length(ψg.func.domain)/div), [ψg.timeStep, ψg.freqStep]), length(ψg.func.domain))
 
 function operator(ψg::ElemFunc)
-    M, N = dimensions(ψg)
+    M, N, _ = dimensions(ψg)
     shift_values = [ψg(m, n).values for m in 0:M-1, n in 0:N-1]
-    sum([shift_g*transpose(shift_g) for shift_g in shift_values])
+    sum([v*transpose(v) for v in shift_values])
 end
 
 netΔ(A, B) = sum([abs(A[i]-B[i]) for i in 1:length(A)])
 
 # returns the netΔ to the L×L identity of the outer product sum
 function biorthogonal(ψg::ElemFunc, ψγ::ElemFunc)
-    if (ψg.timeStep ≠ ψγ.timeStep) || (ψg.freqStep ≠ ψγ.timeStep) || (ψg.func.space ≠ ψγ.func.space)
+    if (dimensions(ψg) ≠ dimensions(ψγ)) 
         throw(FunctionsDoNotMatch())
     end
-    L = size(ψg.func.space)
-    M, N = dimensions(ψg)
+    L = length(ψg.func.domain)
+    M, N, _ = dimensions(ψg)
     out_prod = sum([ψg(m, n).values*conj(transpose(ψγ(m, n).values)) for m in 0:M-1, n in 0:N-1])
     id_L = Matrix{Scalar}(LinearAlgebra.I, L, L)
     netΔ(id_L, out_prod)
@@ -91,37 +103,61 @@ end
 
 function frame(ψg::ElemFunc)
     S = operator(ψg)
-    γ = Func(ψg.func.space, inv(S)*ψg.func.values)
-    ψγ = ElemFunc(γ, ψg.timeStep, ψg.freqStep)
+    γ = inv(S)*ψg.func
+    ψγ = ElemFunc(γ, ψg)
     biorthogonal(ψg, ψγ)
+end
+
+struct Lattice
+    values::Array{Scalar, 2}
+end
+(c::Lattice)(m::Int, n::Int) = c.values[m+1, n+1]
+size(c::Lattice) = size(c.values)
+
+function analyse(ψg::ElemFunc, x::Func)
+    M, N, _ = dimensions(ψg)
+    if (x.domain ≠ ψg.func.domain)
+        throw(AnalyseMismatch())
+    end
+    Σ(m,n) = sum([conj(ψg(m, n)(k))*x(k) for k in ψg.func.domain])
+    Lattice([Σ(m,n) for m in 0:M-1, n in 0:N-1])
+end
+
+function synthesize(ψg::ElemFunc, c::Lattice)
+    M, N, _ = dimensions(ψg)
+    if (M,N) ≠ size(c)
+        throw(SynthesizeMismatch())
+    end
+    x(k) = sum([c(m,n)*ψg(m,n)(k) for m in 0:M-1, n in 0:N-1])
+    domain = ψg.func.domain
+    Func(domain, map(x, domain))
 end
 
 # compute Wexler-Raz minimum energy dual
 function wr_bio(ψg::ElemFunc)::ElemFunc
-    L = size(ψg.func.space)
-    M, N = dimensions(ψg)
-    W = exp(2π*im/N)
-    ρ = M*N/L
-    μ = vcat(1, zeros(Scalar, L-1))
+    L = length(ψg.func.domain)
+    M, N, _ = dimensions(ψg)
     lattice = [(m,n) for m in 0:M-1 for n in 0:N-1]
-    G = [ρ*conj(ψg(m, n)(k)) for (m,n) in lattice, k in ψg.func.space.domain]
-    γ = Func(ψg.func.space, G \ μ)
-    ElemFunc(γ, ψg.timeStep, ψg.freqStep)
+    G = [conj(ψg(m, n)(k)) for (m,n) in lattice, k in ψg.func.domain]
+    μ = vcat(L/(M*N), zeros(Scalar, L-1))
+    γ = Func(ψg.func.domain, G \ μ)
+    ElemFunc(γ, ψg)
 end
 
 end
 
 
-L = 64
-periodic(k::Integer, period) = ((k%period)+period)%period
-window(k::Integer)::Complex{Float64} = (2^(1/2)/8)^(1/2)*exp(-π*((periodic(k,L)-31.5)/8)^2)
+domain = 0:11
+L = length(domain)
 
-space = Gabor.Space(0:63)
-g = Gabor.Func(space, map(window, 0:63))
-g_elem = Gabor.ElemFunc(g, 8, 8)
-S = Gabor.operator(g_elem)
-g̃ = Gabor.Func(space, inv(S)*g.values)
-g̃_elem = Gabor.ElemFunc(g̃, 8, 8)
 
-γ_elem = Gabor.wr_bio(g_elem)
-γ = γ_elem.func
+window(k::Integer)::Complex{Float64} = (2^(1/2)/8)^(1/2)*exp(-(2k)^2)
+
+g = Gabor.Func(domain, map(window, domain))
+ψg = Gabor.ElemFunc(g, 1, 12)
+S = Gabor.operator(ψg)
+g̃ = inv(S)*g
+ψg̃ = Gabor.ElemFunc(g̃, ψg.timeStep, ψg.freqStep)
+
+# ψγ = Gabor.wr_bio(ψg)
+# γ = ψγ.func
